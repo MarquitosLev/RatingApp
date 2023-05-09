72c6d73d21276047f13b6f5678945b3914266794
Marcos Leiva <marcleiva623@gmail.com>
2023-05-09 10:48:29 -0300

    Thank Section end

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const rateButtons = document.querySelectorAll(".rate-button");
 const submitButton = document.querySelector("#submit-button");
+const thanksSection = document.getElementById("th-section");
+const ratingSection = document.getElementById("rat-section");
 let selectedValue = null;
 
 // Realiza la releccion del rating
@@ -23,7 +25,20 @@ rateButtons.forEach((button) => {
   });
 });
 
+// Presional boton submit
 submitButton.addEventListener("click", () => {
-  console.log("Valor seleccionado:", selectedValue);
-  
+  // falta comprobar si hay algun rating selecionado, si no se selecciona, no avanza
+  let num;
+  rateButtons.forEach((button) => {
+    if (button.classList.contains("selected")) {
+      num = button.dataset.value;
+    }
+  });
+  if (num) {
+    thanksSection.querySelector("div:nth-child(2)").textContent =
+      "You selected " + num + " out of 5";
+  }
+  thanksSection.removeAttribute("hidden");
+  thanksSection.style.display = "block";
+  ratingSection.setAttribute("hidden", "true");
 });
